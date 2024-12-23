fb0407adf1c265499fe7cc5751addd34ed000753
JArca-K <jyotirmay.arcane@outlook.com>
2024-12-23 21:20:22 +0530

    Partial implementation of SinglyLinkedList

--- src/main/java/io/jarcak/ds/list/SinglyLinkedList.java
+++ src/main/java/io/jarcak/ds/list/SinglyLinkedList.java
@@ -2,25 +2,46 @@ package io.jarcak.ds.list;
 
 public class SinglyLinkedList<E extends Comparable<E>> implements LinkedList<E>{
 
+    private static class Node<E extends Comparable<E>>{
+
+        private E data;
+
+        private Node<E> next;
+
+        private Node(E data, Node<E> next){
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    private int size;
+
+    private Node<E> head;
+
+    private Node<E> tail;
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public E first() {
-        return null;
+        if(isEmpty())
+            return null;
+        return head.data;
     }
 
     @Override
     public E last() {
-        return null;
+        if(isEmpty())
+            return null;
+        return tail.data;
     }
 
     @Override
